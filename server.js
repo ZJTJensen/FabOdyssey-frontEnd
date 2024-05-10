22db9d0276f002b60321d0c28750a76ebc5da809
ZJTJensen <ZJTJensen@yahoo.com>
2024-05-09 21:25:19 -0400

    sad commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/fab-adventure-game'));
+app.use(express.static('./dist/fabodyssey-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/fab-adventure-game/'}),
+    res.sendFile('index.html', {root: 'dist/fabodyssey-frontend/'}),
 );
 
 app.listen(process.env.PORT || 8080);
