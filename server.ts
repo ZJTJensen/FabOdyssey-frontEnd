d18648a4815f1c1614008df1fb256bfa7b9c1f0b
unknown <zjtjensen@yahoo.com>
2024-05-12 16:01:19 -0400

    feat: trying something new

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
@@ -25,7 +25,7 @@ export function app(): express.Express {
   }));
 
   // All regular routes use the Angular engine
-  server.get('*', (req: Request, res: Response, next) => {
+  server.get('*', (req: Request, res: Response, next: NextFunction) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
     commonEngine
