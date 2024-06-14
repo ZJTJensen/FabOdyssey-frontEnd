25f3146e4183f6dee5f9e7c4f7d5e97e78b9ecd8
ZJTJensen <ZJTJensen@yahoo.com>
2024-06-13 22:37:13 -0400

    feat: map working now just adding post clickable events

--- src/app/world-map/world-map.component.ts
+++ src/app/world-map/world-map.component.ts
@@ -17,40 +17,53 @@ export class WorldMapComponent implements AfterViewInit {
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       let img = this.image.nativeElement;
-      let clickableAreas = document.querySelectorAll('.clickable-area');
-      this.makeImageDraggable(img, clickableAreas);
+      this.makeImageDraggable(img);
     }
   }
 
-  makeImageDraggable(img: any, clickableAreas: any) {
-    let shiftX: number, shiftY: number, isMoving = false;
-    let prevLeft: number = img.getBoundingClientRect().left;
-    let prevTop: number = img.getBoundingClientRect().top;  
+  makeImageDraggable(img: any) {
+    let shiftX: number, shiftY: number, isMoving = false; 
 
     img.onmousedown = (event: MouseEvent) => {
       isMoving = true;
       shiftX = event.clientX - img.getBoundingClientRect().left;
       shiftY = event.clientY - img.getBoundingClientRect().top;
-
       img.style.position = 'absolute';
       img.style.zIndex = '1000';
     };
-
+    
+    let clickableAreas = Array.from(document.querySelectorAll('.clickable-area')).map(area => {
+      return {
+        element: area,
+        prevLeft: area.getBoundingClientRect().left,
+        prevTop: area.getBoundingClientRect().top
+      };
+    });
+    
     document.onmousemove = (event: MouseEvent) => {
       if (!isMoving) return;
+    
       let newLeft = event.pageX - shiftX;
       let newTop = event.pageY - shiftY;
-      let diffLeft = newLeft - prevLeft;
-      let diffTop = newTop - prevTop;
-  
+    
       img.style.left = newLeft + 'px';
-      img.style.top = newTop + 'px';  
+      img.style.top = newTop + 'px';
+    
       clickableAreas.forEach((clickableArea: any) => {
-        let areaLeft = parseFloat(clickableArea.style.left || '0');
-        let areaTop = parseFloat(clickableArea.style.top || '0');
-  
-        clickableArea.style.left = (areaLeft + diffLeft) + 'px';
-        clickableArea.style.top = (areaTop + diffTop) + 'px';
+        let diffLeft = clickableArea.prevLeft - newLeft;
+        let diffTop = clickableArea.prevTop - newTop;
+        let areaLeft = parseFloat(clickableArea.element.style.left || '0');
+        let areaTop = parseFloat(clickableArea.element.style.top || '0');
+        if( (newLeft < 0 || newTop < 0) && (clickableArea.prevLeft + newLeft < 0 || clickableArea.prevTop + newTop < 0)) {
+          clickableArea.element.style.left = (areaLeft - diffLeft) + 'px';
+          clickableArea.element.style.top = (areaTop - diffTop) + 'px';
+        } else {
+          clickableArea.element.style.left = ((areaLeft - diffLeft) > 0 ? areaLeft - diffLeft : diffLeft - areaLeft) + 'px';
+          clickableArea.element.style.top = ((areaTop - diffTop) > 0 ? areaTop - diffTop : diffTop - areaTop) + 'px';
+        }
+        clickableArea.prevLeft = newLeft;
+        clickableArea.prevTop = newTop;
+       clickableArea.element.addEventListener('click', () => this.handleClick(clickableArea.element.id));
       });
     };
 
